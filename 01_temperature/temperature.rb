37bc0961e5d94c1528de7f724420b6e5b6e9cc1d
Erika Keith <erikakeith13@gmail.com>
2018-10-02 23:48:01 +0000

    temp

--- 01_temperature/temperature.rb
+++ 01_temperature/temperature.rb
@@ -1,1 +1,9 @@
-#write your code here
+def main
+    puts "Enter a temperature"
+    temperature = gets.chomp
+    fahrenheit = (temperature.to_i*9/5)+32
+    puts "the equivalent in fahrenheit is: "
+    print fahrenheit
+puts ""
+end
+main if __FILE__==$PROGRAM_NAME 
