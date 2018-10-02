f4f035561abe0ddfdeceec42ae2f9d6c42662ccc
Erika Keith <erikakeith13@gmail.com>
2018-10-02 23:20:40 +0000

    complete

--- 00_hello/hello.rb
+++ 00_hello/hello.rb
@@ -1,1 +1,17 @@
-#write your code here
+def hello(name)
+    if name == ""
+        puts "Hello#{name}"
+    elsif name == "Cassie"
+  puts "Hello Cassie"  
+  else puts "Hello World"
+    end
+end
+
+def main
+    print "What is your name?"
+    name = gets.chomp
+    hello(name)
+end
+
+
+main if __FILE__==$PROGRAM_NAME 
