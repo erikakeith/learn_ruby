4b00415f93cd60f6631214fad587567c660e9eca
Erika Keith <erikakeith13@gmail.com>
2018-10-04 23:53:58 +0000

    calculator

--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -1,1 +1,20 @@
-#write your code here
+def  calculator
+def add(x)
+    x+x
+end
+def subtract(x, y)
+    x-y
+end
+def sum()
+    ""
+end
+def sum(x)
+    ""
+end
+def sum(x, y)
+    ""
+end
+def sum(x, y, z)
+    ""
+end
+end
