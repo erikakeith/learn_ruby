2512f7276e814bad644a2c32d287d3a9dea45feb
Erika Keith <erikakeith13@gmail.com>
2018-10-06 01:19:02 +0000

    updated

--- 04_pig_latin/pig_latin.rb
+++ 04_pig_latin/pig_latin.rb
@@ -1,1 +1,14 @@
-#write your code here
+def pig_latin
+    def translate(text)
+vowels = [a, e, i, o, u]
+consonants = ("a".."z").to_a - vowels
+return text + "ay" if vowels.include?(text[0])
+    end
+return text + "ay" if consonants.include?(text[0])
+if consonants.include?(text[0]) && consonants.include?(text[1])
+    return text[2..-1] + text[0..1] + 'ay'
+end
+if consonants.include?(text[0]) && consonants.include?(text[1]) && consonants.include?(text[2])
+return text[3..-1] + text[0..1] + 'ay'
+end
+end
